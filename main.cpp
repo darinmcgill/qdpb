cd554cda0800b693583d1c877aa9bf8e93a8017f
Darin McGill <darin.mcgill@gmail.com>
2019-02-18 04:55:13 -0500

    tweaks

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <cstring>
 #include "qdpb.h"
 
+char* leak_copy(const char* ptr, const char* end= nullptr) {
+    if (not end)
+        end = strlen(ptr) + ptr;
+    size_t size = end - ptr;
+    auto out = (char*) malloc(size + 1);
+    *(out + size) = 0;
+    memcpy(out, ptr, end - ptr);
+    return out;
+}
 
 int main()
 {
@@ -18,14 +28,25 @@ int main()
     cout << "=>" << as<int64_t>(p) << "<=" << endl;
     cout << endl;
      */
+
     auto m2 = (char*) "\xac\x02\xad\x02";
     auto m3 = (char*) "\x12\x07\x74\x65\x73\x74\x69\x6e\x67";
+    auto m4 = "\x12\x07\x74\x65\x73\x74\x69\x6e\x67\x08\x07\x1a\x03\x11\x12\x13";
     qdpb::Chomper chomper;
-    chomper.chomp(m3);
-    char* m4;
+    chomper.chomp(m4);
+    char* c;
     size_t s;
-    auto hit = chomper.get(2, m4, s);
+    auto hit = chomper.get(2, c, s);
     cout << "hit: " << hit << endl;
-    cout << "=>" << std::string(m4, s) << "<=" << endl;
+    cout << "=>" << std::string(c, s) << "<=" << endl;
+    cout << chomper.get<int>(1) << endl;
     chomper.reset();
+    auto m5 = leak_copy(m4);
+    chomper.chomp(m5);
+    hit = chomper.get(2, c, s);
+    cout << "hit: " << hit << endl;
+    cout << "=>" << c << "<=" << endl;
+    cout << chomper.get<int>(1) << endl;
+    chomper.unpack(3,0,true);
+    cout << chomper.get<int>(3) << endl;
 }
--- qdpb.cpp
+++ qdpb.cpp
@@ -1,7 +1,19 @@
+#include <iostream>
 #include "qdpb.h"
 using namespace qdpb;
+const uint64_t _mask = ((~0ULL) << 16) >> 16;
 
-uint64_t devar(byte_p& ptr, bool clear=false) {
+packed_t pack(const void *p, size_t d = 0)
+{ return (reinterpret_cast<packed_t>(p) & _mask) | d << 48; }
+
+size_t get_size(packed_t packed)
+{ return static_cast<size_t>((packed >> 48) & 0xffff); }
+
+char* get_ptr(packed_t packed)
+{ return reinterpret_cast<char *>(static_cast<int64_t>(packed << 16) >> 16); }
+
+
+uint64_t devar(byte_p& ptr, bool clear) {
     uint64_t out = 0;
     int phase = 0;
     while (true) {
@@ -19,31 +31,26 @@ uint64_t devar(byte_p& ptr, bool clear=false) {
     return out;
 }
 
-void Chomper::reset() {
-    for (auto& vec: maw)
-        vec.clear();
-}
-
 bool Chomper::bite(byte_p& ptr) {
-    auto one = devar(ptr);
+    auto one = devar(ptr, mutate_);
     if (not one)
         return false;
     auto wire_type = one & 0x07;
     auto field_number = one >> 3;
-    if (maw.size() < field_number)
-        maw.resize(field_number);
-    std::vector<packed_t>& tooth = maw[field_number - 1];
+    if (maw_.size() < field_number)
+        maw_.resize(field_number);
+    std::vector<packed_t>& tooth = maw_[field_number - 1];
     uint64_t len;
     switch (wire_type) {
         case 0:
-            tooth.push_back(devar(ptr));
+            tooth.push_back(devar(ptr, mutate_));
             break;
         case 1:
             tooth.push_back(*reinterpret_cast<uint64_t*>(ptr));
             ptr += 8;
             break;
         case 2:
-            len = devar(ptr);
+            len = devar(ptr, mutate_);
             tooth.push_back(pack(ptr, len));
             ptr += len;
             break;
@@ -57,13 +64,61 @@ bool Chomper::bite(byte_p& ptr) {
     return true;
 }
 
-void Chomper::chomp(byte_p start, byte_p ender)
+void Chomper::chomp(char* ptr, byte_p ender, bool mutate)
 {
+    for (auto& vec: maw_)
+        vec.clear();
+    mutate_ = mutate;
+    std::cerr << "mutating chomp:" << mutate << std::endl;
+    start_ = ptr;
     while (true) {
-        if (ender and start >= ender)
+        if (ender and ptr >= ender)
             break;
-        bool more = bite(start);
+        bool more = bite(ptr);
         if (not more)
             break;
     }
+    ender_ = ptr;
+}
+
+void Chomper::chomp(const char* start, byte_p ender)
+{
+    std::cerr << "const chomp" << std::endl;
+    chomp((char*) start, ender, false);
+}
+
+
+bool Chomper::get(int field_number, byte_p &ptr, size_t &size)
+{
+    if (field_number > maw_.size())
+        return false;
+    std::vector<packed_t> &tooth = maw_[field_number - 1];
+    if (tooth.empty())
+        return false;
+    packed_t p = tooth.back();
+    size = get_size(p);
+    ptr = get_ptr(p);
+    return true;
+}
+
+packed_t Chomper::_get(int field_number)
+{
+    if (field_number > maw_.size())
+        return 0;
+    std::vector<packed_t> &tooth = maw_[field_number - 1];
+    if (tooth.empty())
+        return 0;
+    return tooth.back();
+}
+
+void Chomper::unpack(int field_number, int wire_type)
+{
+    byte_p ptr;
+    size_t size;
+    bool has = get(field_number, ptr, size);
+    if (not has)
+        return;
+    if (ptr < start_ or ptr > ender_)
+        throw std::runtime_error("can't unpack");
+    throw std::runtime_error("not implemented");
 }
