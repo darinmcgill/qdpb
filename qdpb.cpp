08d956ea77cd6ff9eb705cf2215cebb5c045d72a
Darin McGill <darin.mcgill@gmail.com>
2019-02-18 22:14:07 -0500

    tweaks

--- qdpb.cpp
+++ qdpb.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
+#include <sstream>
 #include "qdpb.h"
 using namespace qdpb;
 const uint64_t _mask = ((~0ULL) << 16) >> 16;
 
-packed_t pack(const void *p, size_t d = 0)
-{ return (reinterpret_cast<packed_t>(p) & _mask) | d << 48; }
+misc_t squish(const void *p, size_t d = 0)
+{ return (reinterpret_cast<misc_t>(p) & _mask) | d << 48; }
 
-size_t get_size(packed_t packed)
-{ return static_cast<size_t>((packed >> 48) & 0xffff); }
+size_t get_size(misc_t misc)
+{ return static_cast<size_t>((misc >> 48) & 0xffff); }
 
-char* get_ptr(packed_t packed)
-{ return reinterpret_cast<char *>(static_cast<int64_t>(packed << 16) >> 16); }
+char* get_ptr(misc_t misc)
+{ return reinterpret_cast<char *>(static_cast<int64_t>(misc << 16) >> 16); }
 
 
-uint64_t devar(byte_p& ptr, bool clear) {
+uint64_t read_varint(byte_p &ptr, bool clear) {
     uint64_t out = 0;
     int phase = 0;
     while (true) {
@@ -31,42 +32,47 @@ uint64_t devar(byte_p& ptr, bool clear) {
     return out;
 }
 
-bool Chomper::bite(byte_p& ptr) {
-    auto one = devar(ptr, mutate_);
-    if (not one)
-        return false;
-    auto wire_type = one & 0x07;
-    auto field_number = one >> 3;
-    if (maw_.size() < field_number)
-        maw_.resize(field_number);
-    std::vector<packed_t>& tooth = maw_[field_number - 1];
+misc_t read_value(WireType wire_type, byte_p &ptr, bool mutate) {
     uint64_t len;
+    misc_t out;
     switch (wire_type) {
-        case 0:
-            tooth.push_back(devar(ptr, mutate_));
-            break;
-        case 1:
-            tooth.push_back(*reinterpret_cast<uint64_t*>(ptr));
+        case WireType::VARINT:
+            return read_varint(ptr, mutate);
+        case WireType::DOUBLE:
+            out = *reinterpret_cast<uint64_t*>(ptr);
             ptr += 8;
             break;
-        case 2:
-            len = devar(ptr, mutate_);
-            tooth.push_back(pack(ptr, len));
+        case WireType::PASCAL:
+            len = read_varint(ptr, mutate);
+            out = squish(ptr, len);
             ptr += len;
             break;
-        case 5:
-            tooth.push_back(*reinterpret_cast<uint32_t*>(ptr));
+        case WireType::SINGLE:
+            out = *reinterpret_cast<uint32_t*>(ptr);
             ptr += 4;
             break;
         default:
             throw std::runtime_error("bad wire_type:" + std::to_string(wire_type));
     }
+    return out;
+}
+
+bool Parser::read_field(byte_p &ptr) {
+    auto one = read_varint(ptr, mutate_);
+    if (not one)
+        return false;
+    auto wire_type = static_cast<WireType>(one & 0x07);
+    auto field_number = one >> 3;
+    if (vecs_.size() < field_number)
+        vecs_.resize(field_number);
+    std::vector<misc_t>& dest = vecs_[field_number - 1];
+    dest.push_back(read_value(wire_type, ptr, mutate_));
     return true;
 }
 
-void Chomper::chomp(char* ptr, byte_p ender, bool mutate)
+void Parser::parse(char *ptr, byte_p ender, bool mutate)
 {
-    for (auto& vec: maw_)
+    for (auto& vec: vecs_)
         vec.clear();
     mutate_ = mutate;
     std::cerr << "mutating chomp:" << mutate << std::endl;
@@ -74,51 +80,92 @@ void Chomper::chomp(char* ptr, byte_p ender, bool mutate)
     while (true) {
         if (ender and ptr >= ender)
             break;
-        bool more = bite(ptr);
+        bool more = read_field(ptr);
         if (not more)
             break;
     }
     ender_ = ptr;
 }
 
-void Chomper::chomp(const char* start, byte_p ender)
+void Parser::parse(const char *start, byte_p ender)
 {
     std::cerr << "const chomp" << std::endl;
-    chomp((char*) start, ender, false);
+    parse((char *) start, ender, false);
 }
 
 
-bool Chomper::get(int field_number, byte_p &ptr, size_t &size)
+bool Parser::get_string(int field_number, byte_p &ptr, size_t &size, int index) const
 {
-    if (field_number > maw_.size())
+    if (field_number > vecs_.size())
         return false;
-    std::vector<packed_t> &tooth = maw_[field_number - 1];
-    if (tooth.empty())
-        return false;
-    packed_t p = tooth.back();
+    const std::vector<misc_t> &field_vec = vecs_[field_number - 1];
+    misc_t p;
+    if (index < 0)
+    {
+        if (field_vec.empty())
+            return false;
+        p = field_vec.back();
+    } else {
+        p = field_vec.at((unsigned long) index);
+    }
     size = get_size(p);
     ptr = get_ptr(p);
     return true;
 }
 
-packed_t Chomper::_get(int field_number)
+misc_t Parser::get_misc(int field_number, int index) const
 {
-    if (field_number > maw_.size())
+    if (field_number > vecs_.size())
         return 0;
-    std::vector<packed_t> &tooth = maw_[field_number - 1];
-    if (tooth.empty())
-        return 0;
-    return tooth.back();
+    const std::vector<misc_t> &field_vec = vecs_[field_number - 1];
+    if (index < 0) {
+        if (field_vec.empty())
+            return 0;
+        return field_vec.back();
+    } else {
+        return field_vec.at(static_cast<size_t>(index));
+    }
 }
 
-void Chomper::unpack(int field_number, int wire_type)
+void Parser::unpack(int field_number, WireType wire_type)
 {
     byte_p ptr;
     size_t size;
-    bool has = get(field_number, ptr, size);
-    if (not has)
+    bool present = get_string(field_number, ptr, size);
+    byte_p end = ptr + size;
+    if (not present)
         return;
     if (ptr < start_ or ptr > ender_)
-        throw std::runtime_error("can't unpack");
-    throw std::runtime_error("not implemented");
+        throw std::runtime_error("can't unpack non-string");
+    std::vector<misc_t> &dest = vecs_[field_number - 1];
+    dest.clear();
+    while (ptr < end) {
+        dest.push_back(read_value(wire_type, ptr, mutate_));
+    }
+}
+
+std::string Parser::dump()
+{
+    std::stringstream ss;
+    ss << "{";
+    for (int i=0;i<vecs_.size();i++) {
+        auto& vec = vecs_[i];
+        if (not vec.empty()) {
+            ss << i << ":[";
+            for (auto e: vec)
+            {
+                auto ptr = get_ptr(e);
+                auto size = get_size(e);
+                if (ptr >= start_ and ptr <= ender_) {
+                    ss << "'" << std::string(ptr, size) << "',";
+                } else {
+                    ss << e << ",";
+                }
+
+            }
+            ss << "],";
+        }
+    }
+    ss << "}";
+    return ss.str();
 }
--- qdpb.h
+++ qdpb.h
@@ -7,31 +7,40 @@
 
 namespace qdpb
 {
-    using packed_t = uint64_t;
+    using misc_t = uint64_t;
     using byte_p = char *;
 
-    class Chomper
+    enum WireType {
+        VARINT=0,
+        DOUBLE=1,
+        PASCAL=2,
+        SINGLE=5
+    };
+
+    class Parser
     {
-        std::vector<std::vector<packed_t>> maw_;
+        std::vector<std::vector<misc_t>> vecs_;
         char* start_;
         char* ender_;
         bool mutate_;
-        packed_t _get(int field_number);
-        bool bite(byte_p &ptr);
+        misc_t get_misc(int field_number, int index=-1) const;
+        bool read_field(byte_p &ptr);
 
     public:
 
-        void chomp(char*, byte_p ender = nullptr, bool mutate=true);
-        void chomp(const char*, byte_p ender = nullptr);
+        void parse(char *ptr, byte_p ender = nullptr, bool mutate = true);
+        void parse(const char *ptr, byte_p ender = nullptr);
 
         template<class T>
-        T get(int field_number) {
-            packed_t p = _get(field_number);
+        T get_value(int field_number, int index=-1) const {
+            misc_t p = get_misc(field_number, index);
             return *reinterpret_cast<T *>(&p);
         }
 
-        bool get(int field_number, byte_p& ptr, size_t& size);
+        bool get_string(int field_number, byte_p &ptr, size_t &size, int index=-1) const;
 
-        void unpack(int field_number, int wire_type);
+        void unpack(int field_number, WireType wire_type);
+
+        std::string dump();
     };
 }
